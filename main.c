0df07809ba74b9b24d174d0d34fbc6b9506e8940
DownloadFirefox <61615730+uimaxbai@users.noreply.github.com>
2023-07-04 12:52:29 +0000

    yay

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 #include "util.h"
 #include <string.h>
 
+// READ if you do not know ncurses: https://medium.com/programming-in-c/an-introduction-to-ncurses-in-c-d977efd706f8
+
+
 /*
 Printing using ncurses:
 - printw() - equivalent to printf(), for variables
@@ -11,19 +14,47 @@ Printing using ncurses:
 */
 
 int main() {
+    initscr();
+    helloworld();
+    colours();
+    endwin();
+    return EXIT_SUCCESS;
+}
+
+int helloworld() {
 
     // Initialize ncurses
-    initscr();
     // util.h: void clr()
     // Clears screen
     clr();
     addstr("Hello, world!\n");
     refresh(); // Refresh the screen to show the latest content
-
+    addstr("Press any key to exit.");
     // pause the screen output, wait for key input to continue
     getch();
 
     // deallocates memory and ends ncurses
-    endwin();
+    return EXIT_SUCCESS;
+}
+
+/*
+This is a full list of ncurses colour constants:
+
+COLOR_BLACK
+COLOR_RED
+COLOR_GREEN
+COLOR_YELLOW
+COLOR_BLUE
+COLOR_MAGENTA
+COLOR_CYAN
+COLOR_WHITE
+*/
+int colours() {
+    clr();
+    if (has_colors()) { // Check if the terminal can output colours
+        if(start_color() == OK) { // Start color and see if it is ok
+
+        }
+    }
     return EXIT_SUCCESS;
 }
